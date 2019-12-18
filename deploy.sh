738a7c1925e95d008cfd8d93b0280988b789c2ae
鬼王 <THRAEX@aliyun.com>
2019-12-18 10:29:50 +0800

    chore: update target

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,15 @@ yarn docs:build
 cd docs/.vuepress/dist
 
 git init
+# git clone git@github.com:O70/O70.github.io.git pages
+# cp -r pages/.git .
+# rm -rf pages
 git add -A
 git commit -m 'deploy'
 
 # git push -f git@github.com:O70/note-paper.git dev:gh-pages
-git push -f git@github.com:O70/O70.github.io.git master:gh-pages
-git push -f git@gitee.com:Guiwang/Guiwang.git master:gh-pages
-git push -f git@git.dev.tencent.com:THRAEX/THRAEX.git master:gh-pages
+git push -f git@github.com:O70/O70.github.io.git master
+git push -f git@gitee.com:Guiwang/Guiwang.git master
+git push -f git@git.dev.tencent.com:THRAEX/THRAEX.git master
 
 cd -
