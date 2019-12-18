a23dbb91b32d77f39677ea20fbb722ba57aa4458
鬼王 <THRAEX@aliyun.com>
2019-12-18 09:46:09 +0800

    chore: deploy to root

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+# Deploy to Root
 
 set -e
 
@@ -10,6 +11,9 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:O70/note-paper.git dev:gh-pages
+# git push -f git@github.com:O70/note-paper.git dev:gh-pages
+git push -f git@github.com:O70/O70.github.io.git master:gh-pages
+git push -f git@gitee.com:Guiwang/Guiwang.git master:gh-pages
+git push -f git@git.dev.tencent.com:THRAEX/THRAEX.git master:gh-pages
 
 cd -
